a7e73e0f279c700cc0ae0e7341dd91376529e31c
ADoige Notebook <agu-amd@hotmail.com>
2019-03-28 18:07:59 -0300

    Added argument execution in console

--- randomRenaming.py
+++ randomRenaming.py
@@ -13,7 +13,6 @@ change the path of line 23 to the directory where the reaname should occur"""
 import os
 import random
 from sys import argv
-arguments = sys.argv
 
 def getRanStr(length):
 	# Produces a string of <lenght> random digits
@@ -22,39 +21,76 @@ def getRanStr(length):
 		ans += str(random.randint(0,9))
 	return ans
 
-def main():
-	cache = []
-	digits = 20
+def manualMessage():
+	print("No arguments passed. Starting manual:\n\n")
+	print("type  'python randomRenaming.py 1'  for basic update or start of a new execution with default settings.")
+	print("type  'python randomRenaming.py 2'  for showcasing custom settings.")
+	print("type  'python randomRenaming.py 3'  for checks for deleted files.")
+	print("type  'python randomRenaming.py 4'  for renaiming of all files.")
 
+def customSettingsMessage():
+	pass
+
+def readCache():
+	cacheR = set()
 	try:
+		print("Reading Cache...")
 		with open('cache.txt','r') as f:
 			#This reads the already cached numbers so new uses don't need a complete re-execution
 			while True:
 				cached_value = f.readline()
 				print(cached_value.rstrip())
-				cache.append(cached_value.rstrip())
+				cacheR.append(cached_value.rstrip())
 				if (cached_value == ''):
+					print("Reading Cache done")
 					break
 	except FileNotFoundError:
+		print("Cache File not found")
 		pass
 
-	with open('cache.txt','a') as g:
-		for it in os.listdir():
-			tupl = os.path.splitext(it) #This splits the name so we get the extension
-			if (it == "cache.txt" or it == "randomRenaming.py" or (tupl[0] in cache)):
-				pass
-			else:
-				random_string = getRanStr(digits)
-				try:
-					os.rename(it,random_string+tupl[1])
-				except Exception:
-					while True:
-						try:
-							#This takes the string, converts it into an Int and adds 1, then turns it back into a string and tries again.
-							random_string = str((int(random_string)+1)).zfill(digits) 
-							os.rename(it,random_string+tupl[1])
-							break
-						except Exception:
-							pass
-				g.write(random_string+"\n")
-				cache.append(random_string)
+def writeCache(cacheSet):
+	cacheToWrite = sorted(list(cacheSet))
+	with open('cache.txt','w') as g:
+		for element in cacheToWrite:
+			g.write(element+"\n")
+
+def start(digits=20,renameEverything=False,saveCache=True):
+	if renameEverything:
+		cache = set()
+	else:
+		cache = readCache()	
+
+	for it in os.listdir():
+		tupl = os.path.splitext(it) #This splits the name so we get the extension
+		if (it == "cache.txt" or it == "randomRenaming.py" or (tupl[0] in cache)):
+			pass
+		else:
+			random_string = getRanStr(digits)
+			try:
+				os.rename(it,random_string+tupl[1])
+			except Exception:
+				while True:
+					try:
+						#This takes the string, converts it into an Int and adds 1, then turns it back into a string and tries again.
+						random_string = str((int(random_string)+1)).zfill(digits) 
+						os.rename(it,random_string+tupl[1])
+						break
+					except Exception:
+						pass
+			cache.add(random_string)
+	if saveCache:
+		writeCache(cache)
+
+arguments = sys.argv
+	if len(arguments) == 1:
+		manualMessage()
+	elif arguments[1] == 1:
+		start()
+	elif arguments[1] == 2:
+		customSettingsMessage()
+	elif arguments[1] == 3:
+		pass
+	elif arguments[1] == 4:
+		start(renameEverything=True)
+
+
